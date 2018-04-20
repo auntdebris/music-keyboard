f179f4a456095e87c63874836e3217ce21fae938
Paula Cunha <pcunha@sapient.com>
2018-04-20 23:46:16 +0100

    Changed mindimension

--- src/trackers/ColorTracker.js
+++ src/trackers/ColorTracker.js
@@ -75,7 +75,7 @@
    * @default 20
    * @type {number}
    */
-  tracking.ColorTracker.prototype.minDimension = 40;
+  tracking.ColorTracker.prototype.minDimension = 20;
 
   /**
    * Holds the maximum dimension to classify a rectangle.
